1b27c9e6579f84ce12df2bdf46e86fe2d3fcb508
Edwin Varghese <127734453+edwinvarghese15@users.noreply.github.com>
2025-04-23 09:54:34 +0530

    Add files via upload

---
+++ evenlimit.sh
@@ -0,0 +1,7 @@
+echo "Enter the value of N:"
+read  N
+echo "Even no from 0 to $N"
+for((i=0;i<=N;i+=2));
+do
+echo "$i"
+done
--- reverse.sh
+++ reverse.sh
@@ -1,12 +1,5 @@
+echo "Enter the string"
+read str
+rev=$(echo "$str" | rev)
+echo "The reversed string is $rev"
 
-echo "Enter a number: " 
-read num
-original_num=$num
-rev=0
-while [ $num -gt 0 ]
-do
-  digit=$((num % 10))
-  rev=$((rev * 10 + digit))
-  num=$((num / 10))
-done
-echo "The reversed number is $rev"
